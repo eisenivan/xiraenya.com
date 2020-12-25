ad9a80b5eb06c2d5f39bf633a34f369c88c86290
eisenivan <ivan.eisenberg@gmail.com>
2020-12-24 20:33:13 -0600

    add gradient and update dev server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3100
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`xiraenya.com listening at http://localhost:${port}`)
 })
